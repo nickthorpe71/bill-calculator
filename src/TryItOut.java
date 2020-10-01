9cea70be0247259422cdc739e7e8dc1d9848d1ae
nickthorpe71 <nickthorpe71@gmail.com>
2020-10-01 12:07:10 -0500

    tests assignment

--- src/TryItOut.java
+++ src/TryItOut.java
@@ -1,5 +1,19 @@
 public class TryItOut {
     public static void main(String[] args) {
         System.out.println("testing");
+
+        int cupsOfCoffeeMonday;
+        int cupsOfCoffeeTuesday;
+        int cupsOfCoffeeWednesday;
+        int cupsOfCoffeeThursday;
+        int cupsOfCoffeeFriday;
+
+        cupsOfCoffeeMonday = 4;
+        cupsOfCoffeeTuesday = 3;
+        cupsOfCoffeeWednesday = 6;
+        cupsOfCoffeeThursday = 5;
+        cupsOfCoffeeFriday = 4;
+
+        System.out.println(cupsOfCoffeeFriday);
     }
 }
