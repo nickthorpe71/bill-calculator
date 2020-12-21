f0ba3112e0d7aadedeb0b871857f06f506da8a1a
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-21 06:34:09 -0600

    adds 7kyu to try it out class

--- src/TryItOut.java
+++ src/TryItOut.java
@@ -1,36 +1,22 @@
+import java.util.Arrays;
+
 public class TryItOut {
-    public static int nthPower(int[] array, int n) {
-        int result = 1;
-        if(n < array.length) {
-            for (int i = 0; i < n; i++)
-                result *= array[n];
-            return result;
-        }
-        return -1;
-    }
+    public static String highAndLow(String numbers) {
 
-    public static String hydrate(String drinkString)
-    {
-        drinkString = drinkString.replaceAll("[^\\d]", " ");
-        drinkString = drinkString.trim();
-        drinkString = drinkString.replaceAll(" ", "");
+        int min = Arrays.stream(numbers.split(" "))
+                .mapToInt(i -> Integer.parseInt(i))
+                .min()
+                .getAsInt();
 
-        int total = 0;
+        int max = Arrays.stream(numbers.split(" "))
+                .mapToInt(i -> Integer.parseInt(i))
+                .max()
+                .getAsInt();
 
-
-
-        for(int i = 0; i < drinkString.length(); i++)
-        {
-            int num = Character.getNumericValue(drinkString.charAt(i));
-            System.out.println(num);
-
-            total += num;
-        }
-
-        return String.format("%d glass%s of water", total, total==1?"":"es");
+        return String.format("%d %d", max, min);
     }
 
     public static void main(String[] args) {
-        System.out.println(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
+        System.out.println(highAndLow("-8 -3 -5 -4 -2 -1 0 0 -9 -4 -7 -4 -4"));
     }
 }
