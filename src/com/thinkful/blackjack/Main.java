d99ae79f0e63f3b38e36f37769cc088195c2ec9a
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-21 06:07:16 -0600

    adds revised card and main classes

--- src/com/thinkful/blackjack/Main.java
+++ src/com/thinkful/blackjack/Main.java
@@ -8,15 +8,16 @@ public class Main {
         System.out.println(aceSpades);
 
         // Try the 2 argument constructor
-        Card sevenSpades = new Card("7", Suit.SPADES);
-        Card jackClubs = new Card("Jack", Suit.CLUBS);
+        Card sevenSpades = new Card(Face.SEVEN, Suit.SPADES);
+        Card jackClubs = new Card(Face.JACK, Suit.CLUBS);
 
         System.out.println(sevenSpades);
         System.out.println(jackClubs);
 
         // try the 4 argument constructor
-        Card eightHearts = new Card("8", Suit.HEARTS, 8, false);
-        Card tenClubs = new Card("10", Suit.CLUBS, 10, true);
+        Card eightHearts = new Card(Face.EIGHT, Suit.HEARTS, 8, false);
+        Card tenClubs = new Card(Face.TEN, Suit.CLUBS, 10, true);
+
         System.out.println(eightHearts);
 
         // print the ten of clubs
@@ -25,7 +26,6 @@ public class Main {
         System.out.println(tenClubs);
         tenClubs.setValue(11); // give this card more value
         System.out.println(tenClubs);
-
 
     }
 }
