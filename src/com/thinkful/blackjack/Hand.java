4580c03f0495f327bf73b214384c93887490c876
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:33:30 -0600

    adds getValue method to Hand class

--- src/com/thinkful/blackjack/Hand.java
+++ src/com/thinkful/blackjack/Hand.java
@@ -17,4 +17,12 @@ public class Hand {
     public void addCard(Card card) {
         this.getCards().add(card);
     }
+
+    public int getValue() {
+        int total = 0;
+        for (Card card: this.getCards()) {
+            total += card.getValue();
+        }
+        return total;
+    }
 }
