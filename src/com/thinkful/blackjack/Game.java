cd7715ac2dfb42c5d6440fe2b850f8a293441d94
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:45:51 -0600

    adds basic game setup to Game class along with toString getters, and setters

--- src/com/thinkful/blackjack/Game.java
+++ src/com/thinkful/blackjack/Game.java
@@ -1,4 +1,49 @@
 package com.thinkful.blackjack;
 
 public class Game {
+
+    private Hand player;
+    private Hand dealer;
+    private Deck deck;
+
+    public Game() {
+        setPlayer(new Hand());
+        setDealer(new Hand());
+        setDeck(new Deck());
+        getDeck().shuffle();
+        getPlayer().addCard(getDeck().deal());
+        getDealer().addCard(getDeck().deal());
+        getPlayer().addCard(getDeck().deal());
+        getDealer().addCard(getDeck().deal());
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Player: %s, Dealer: %s", getPlayer(), getDealer());
+    }
+
+    public Hand getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Hand player) {
+        this.player = player;
+    }
+
+    public Hand getDealer() {
+        return dealer;
+    }
+
+    public void setDealer(Hand dealer) {
+        this.dealer = dealer;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
 }
