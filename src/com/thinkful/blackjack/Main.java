324c47660e816a1e075fbf07b22e481ce3fe0af0
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:47:20 -0600

    adds Action enum

--- src/com/thinkful/blackjack/Main.java
+++ src/com/thinkful/blackjack/Main.java
@@ -2,23 +2,7 @@ package com.thinkful.blackjack;
 
 public class Main {
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.shuffle();
-        System.out.println("This is the shuffled Deck");
-        System.out.println(deck);
-
-        Hand hand = new Hand();
-        System.out.println("The empty Hand:");
-        System.out.println(hand);
-        hand.addCard(deck.deal());
-        System.out.println("After one card is dealt");
-        System.out.println(hand);
-        hand.addCard(deck.deal());
-        System.out.println("After two cards are dealt");
-        System.out.println(hand);
-
-        System.out.println("The deck after two cards are dealt");
-        System.out.println(deck);
-
+        Game game = new Game();
+        System.out.println(game);
     }
 }
