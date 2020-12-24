450e2037364975e1a92a58d627f5a29ace9cebbf
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:17:51 -0600

    adds dynamic list to deck class

--- src/TryItOut.java
+++ src/TryItOut.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class TryItOut {
-    public static String highAndLow(String numbers) {
 
-        int min = Arrays.stream(numbers.split(" "))
-                .mapToInt(i -> Integer.parseInt(i))
-                .min()
-                .getAsInt();
+    public static String order(String words) {
+        String result = "";
+        HashMap<Integer, String> wordsMap = new HashMap<>();
+        String[] splitWords = words.split(" ");
 
-        int max = Arrays.stream(numbers.split(" "))
-                .mapToInt(i -> Integer.parseInt(i))
-                .max()
-                .getAsInt();
+        if(splitWords.length == 0 || splitWords[0] == "")
+            return "";
 
-        return String.format("%d %d", max, min);
+        for (int n = 0; n < splitWords.length; n++ ){
+            for(int i = 0; i < splitWords[n].length(); i++){
+                int currentCharAsInt = splitWords[n].charAt(i) - '0';
+                if(currentCharAsInt < 10)
+                    wordsMap.put(currentCharAsInt, splitWords[n]);
+            }
+        }
+
+        for(int i = 1; i <= splitWords.length; i++) {
+            result += wordsMap.get(i);
+            if (i < splitWords.length)
+                result += " ";
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(highAndLow("-8 -3 -5 -4 -2 -1 0 0 -9 -4 -7 -4 -4"));
+        System.out.println(order(""));
     }
 }
---
+++ src/com/thinkful/blackjack/Deck.java
@@ -0,0 +1,42 @@
+package com.thinkful.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+
+    private List<Card> cards;
+
+    public Deck(){
+        this.setCards(new ArrayList<>());
+        int i = 0;
+        for(Suit suit : Suit.values())
+            for(Face face : Face.values()){
+                this.getCards().add(new Card(face, suit));
+                i++;
+            }
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards){
+        this.cards = cards;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (Card c : this.getCards()) {
+            sb.append(String.format("%s,", c));
+        }
+        if (this.getCards().size() > 0) {
+            sb.deleteCharAt(sb.length() - 1); //remove the extraneous comma
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
