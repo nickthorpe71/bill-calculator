60754874f71e82106fd6d726d44677993d100e7d
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:29:54 -0600

    adds empty constructor to Hand

--- src/com/thinkful/blackjack/Hand.java
+++ src/com/thinkful/blackjack/Hand.java
@@ -1,9 +1,14 @@
 package com.thinkful.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
     private List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<Card>();
+    }
 
     public List<Card> getCards() {
         return cards;
