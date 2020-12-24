c1fd0479b86f2a11776133667ded559699f0cf3c
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-24 10:29:11 -0600

    adds getter for cards in Hand class

--- src/com/thinkful/blackjack/Hand.java
+++ src/com/thinkful/blackjack/Hand.java
@@ -1,4 +1,11 @@
 package com.thinkful.blackjack;
 
+import java.util.List;
+
 public class Hand {
+    private List<Card> cards;
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
